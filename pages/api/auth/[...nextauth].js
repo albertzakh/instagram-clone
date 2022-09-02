2d9ecfc9b740b4d7d96ff33b32016752413c1b83
albertzakh <zakhrabekyanalbert@gmail.com>
2022-09-02 07:54:02 +0000

    instagra clone deploy 1

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,7 +7,6 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      secret: process.env.NEXTAUTH_PUBLIC_SECRET
     }),
   ],
   pages: {
